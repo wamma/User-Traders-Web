7cfa099c5f9d9ad909eb969179102bb7155e9c5a
wamma <pparkheong96@naver.com>
2021-10-03 17:50:58 +0900

    rebase update

--- src/App.vue
+++ src/App.vue
@@ -1,160 +1,160 @@
 <template>
-	<v-app>
-		<div>
-			<v-app-bar
-				flat
-				color="orange accent-4"
-				height="70"
-				class="fixed-bar "
-				fixed
-			>
-				<v-toolbar-title>
-					<v-btn icon @click="$router.go(-1)">
-						<v-icon size="xx-large" color="white">mdi-arrow-left</v-icon>
-					</v-btn>
-				</v-toolbar-title>
+  <v-app>
+    <div>
+      <v-app-bar
+        flat
+        color="orange accent-4"
+        height="70"
+        class="fixed-bar "
+        fixed
+      >
+        <v-toolbar-title>
+          <v-btn icon @click="$router.go(-1)">
+            <v-icon size="xx-large" color="white">mdi-arrow-left</v-icon>
+          </v-btn>
+        </v-toolbar-title>
 
-				<v-toolbar-title class="ma-2">
-					<a
-						href="/"
-						style="text-decoration-line:none ; color:white; font-size : x-large "
-					>
-						User Traders
-					</a>
-				</v-toolbar-title>
+        <v-toolbar-title class="ma-2">
+          <a
+            href="/"
+            style="text-decoration-line:none ; color:white; font-size : x-large "
+          >
+            User Traders
+          </a>
+        </v-toolbar-title>
 
-				<v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-				<div v-if="loginflag">
-					<v-btn icon :to="{ name: 'Cart' }">
-						<v-icon size="xx-large" color="white">mdi-cart</v-icon>
-					</v-btn>
-				</div>
+        <div v-if="loginflag">
+          <v-btn icon :to="{ name: 'Cart' }">
+            <v-icon size="xx-large" color="white">mdi-cart</v-icon>
+          </v-btn>
+        </div>
 
-				<div v-if="!loginflag">
-					<v-menu bottom left>
-						<template v-slot:activator="{ on, attrs }">
-							<v-btn
-								dark
-								icon
-								v-bind="attrs"
-								v-on="on"
-								align-center
-								style="border:solid; border-width:2px; border-color: white;"
-							>
-								<v-icon size="xx-large" color="white">mdi-account-alert</v-icon>
-							</v-btn>
-						</template>
+        <div v-if="!loginflag">
+          <v-menu bottom left>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                dark
+                icon
+                v-bind="attrs"
+                v-on="on"
+                align-center
+                style="border:solid; border-width:2px; border-color: white;"
+              >
+                <v-icon size="xx-large" color="white">mdi-account-alert</v-icon>
+              </v-btn>
+            </template>
 
-						<v-list dense nav>
-							<v-list-item
-								:to="{ name: 'UserLogin' }"
-								active-class="deep-orange--text text--accent-3"
-							>
-								<v-list-item-content>
-									<v-list-item-title
-										align-center
-										justify-center
-										style="font-size : medium; text-align : center"
-									>
-										로그인
-									</v-list-item-title>
-								</v-list-item-content>
-							</v-list-item>
+            <v-list dense nav>
+              <v-list-item
+                :to="{ name: 'UserLogin' }"
+                active-class="deep-orange--text text--accent-3"
+              >
+                <v-list-item-content>
+                  <v-list-item-title
+                    align-center
+                    justify-center
+                    style="font-size : medium; text-align : center"
+                  >
+                    로그인
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
 
-							<v-list-item
-								:to="{ name: 'SignUp' }"
-								active-class="deep-orange--text text--accent-3"
-							>
-								<v-list-item-content>
-									<v-list-item-title
-										align-center
-										justify-center
-										style="font-size : medium; text-align : center"
-									>
-										회원가입
-									</v-list-item-title>
-								</v-list-item-content>
-							</v-list-item>
-							<v-list-item
-								id="kakao-talk-channel-add-button"
-								data-channel-public-id="_paeus"
-								data-size="small"
-								data-support-multiple-densities="true"
-							>
-							</v-list-item>
-						</v-list>
-					</v-menu>
-				</div>
-				<div v-else>
-					<v-menu bottom left>
-						<template v-slot:activator="{ on, attrs }">
-							<v-btn dark icon v-bind="attrs" v-on="on">
-								<v-icon size="xx-large" color="green">mdi-account-check</v-icon>
-							</v-btn>
-						</template>
-						<v-list dense nav>
-							<v-list-item
-								active-class="deep-gray--text text--accent-3"
-								style="cursor : pointer"
-								:to="{ name: 'Profile' }"
-							>
-								<v-list-item-content>
-									<v-list-item-title
-										style="font-size : medium; text-align : center"
-										>내 정보
-										<v-icon large color="white">mdi-information</v-icon>
-									</v-list-item-title>
-								</v-list-item-content>
-							</v-list-item>
+              <v-list-item
+                :to="{ name: 'SignUp' }"
+                active-class="deep-orange--text text--accent-3"
+              >
+                <v-list-item-content>
+                  <v-list-item-title
+                    align-center
+                    justify-center
+                    style="font-size : medium; text-align : center"
+                  >
+                    회원가입
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item
+                id="kakao-talk-channel-add-button"
+                data-channel-public-id="_paeus"
+                data-size="small"
+                data-support-multiple-densities="true"
+              >
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+        <div v-else>
+          <v-menu bottom left>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn dark icon v-bind="attrs" v-on="on">
+                <v-icon size="xx-large" color="green">mdi-account-check</v-icon>
+              </v-btn>
+            </template>
+            <v-list dense nav>
+              <v-list-item
+                active-class="deep-gray--text text--accent-3"
+                style="cursor : pointer"
+                :to="{ name: 'Profile' }"
+              >
+                <v-list-item-content>
+                  <v-list-item-title
+                    style="font-size : medium; text-align : center"
+                    >내 정보
+                    <v-icon large color="white">mdi-information</v-icon>
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
 
-							<v-list-item
-								active-class="deep-gray--text text--accent-3"
-								style="cursor : pointer"
-							>
-								<v-list-item-content>
-									<v-list-item-title
-										@click="userLogout"
-										style="font-size : medium; text-align : center"
-										>로그아웃
-										<v-icon>mdi-logout</v-icon>
-									</v-list-item-title>
-								</v-list-item-content>
-							</v-list-item>
-						</v-list>
-					</v-menu>
-				</div>
-			</v-app-bar>
-		</div>
-		<v-spacer></v-spacer>
-		<v-main>
-			<router-view></router-view>
-		</v-main>
-		<v-bottom-navigation height="60" style="" color="#ff6d00" fixed>
-			<v-btn :to="{ name: 'Home' }">
-				<span style="color : #00000099; font-size : medium">홈</span>
-				<v-icon large>mdi-home</v-icon>
-			</v-btn>
-			<div v-if="loginflag">
-				<v-btn>
-					<span style="color : #00000099;  font-size : medium">채팅</span>
-					<v-icon large>mdi-chat</v-icon>
-				</v-btn>
-			</div>
+              <v-list-item
+                active-class="deep-gray--text text--accent-3"
+                style="cursor : pointer"
+              >
+                <v-list-item-content>
+                  <v-list-item-title
+                    @click="userLogout"
+                    style="font-size : medium; text-align : center"
+                    >로그아웃
+                    <v-icon>mdi-logout</v-icon>
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+      </v-app-bar>
+    </div>
+    <v-spacer></v-spacer>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <v-bottom-navigation height="60" style="" color="#ff6d00" fixed>
+      <v-btn :to="{ name: 'Home' }">
+        <span style="color : #00000099; font-size : medium">홈</span>
+        <v-icon large>mdi-home</v-icon>
+      </v-btn>
+      <div v-if="loginflag">
+        <v-btn>
+          <span style="color : #00000099;  font-size : medium">채팅</span>
+          <v-icon large>mdi-chat</v-icon>
+        </v-btn>
+      </div>
 
-			<v-btn :to="{ name: 'Search' }">
-				<span style="color : #00000099; font-size : medium">검색</span>
-				<v-icon large>mdi-shopping-search</v-icon>
-			</v-btn>
+      <v-btn :to="{ name: 'Search' }">
+        <span style="color : #00000099; font-size : medium">검색</span>
+        <v-icon large>mdi-shopping-search</v-icon>
+      </v-btn>
 
-			<div v-if="loginflag">
-				<v-btn :to="{ name: 'Create' }">
-					<span style="color : #00000099; font-size : medium">중고거래</span>
-					<v-icon large color="blue">mdi-plus-circle-outline</v-icon>
-				</v-btn>
-			</div>
+      <div v-if="loginflag">
+        <v-btn :to="{ name: 'Create' }">
+          <span style="color : #00000099; font-size : medium">중고거래</span>
+          <v-icon large color="blue">mdi-plus-circle-outline</v-icon>
+        </v-btn>
+      </div>
 
-			<!-- <div v-if="loginflag">
+      <!-- <div v-if="loginflag">
 				<v-btn @click="overlay = !overlay">
 					<span style="color : #00000099; font-size : medium">카카오톡</span>
 					<v-icon large color="blue">mdi-plus-circle-outline</v-icon></v-btn
@@ -165,73 +165,73 @@
 					</v-btn>
 				</v-overlay>
 			</div> -->
-		</v-bottom-navigation>
-	</v-app>
+    </v-bottom-navigation>
+  </v-app>
 </template>
 
 <script>
 window.kakaoAsyncInit = function() {
-	Kakao.Channel.createAddChannelButton({
-		container: '#kakao-talk-channel-add-button',
-	});
+  Kakao.Channel.createAddChannelButton({
+    container: "#kakao-talk-channel-add-button",
+  });
 };
 
 (function(d, s, id) {
-	var js,
-		fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) return;
-	js = d.createElement(s);
-	js.id = id;
-	js.src = 'https://developers.kakao.com/sdk/js/kakao.channel.min.js';
-	fjs.parentNode.insertBefore(js, fjs);
-})(document, 'script', 'kakao-js-sdk');
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://developers.kakao.com/sdk/js/kakao.channel.min.js";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "kakao-js-sdk");
 
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions } from "vuex";
 
 export default {
-	data: () => ({
-		overlay: false,
-		openMenu: false,
-		loginflag: false,
-	}),
-	mounted() {},
+  data: () => ({
+    overlay: false,
+    openMenu: false,
+    loginflag: false,
+  }),
+  mounted() {},
 
-	computed: {
-		...mapState({
-			isLogin: (state) => state.users.isLogin,
-			isLoginError: (state) => state.users.isLoginError,
-		}),
-	},
-	mounted() {
-		this.loginCheck();
-	},
-	methods: {
-		loginCheck() {
-			if (!localStorage.getItem('user')) {
-				this.loginflag = false;
-			} else {
-				this.loginflag = true;
-			}
-		},
+  computed: {
+    ...mapState({
+      isLogin: (state) => state.users.isLogin,
+      isLoginError: (state) => state.users.isLoginError,
+    }),
+  },
+  mounted() {
+    this.loginCheck();
+  },
+  methods: {
+    loginCheck() {
+      if (!localStorage.getItem("user")) {
+        this.loginflag = false;
+      } else {
+        this.loginflag = true;
+      }
+    },
 
-		userLogout() {
-			this.getUserLogout().then(() => {
-				this.isLoading = false;
-				localStorage.removeItem('user');
-				this.$router.push({ name: 'UserLogin' });
-			});
-		},
-		...mapActions({
-			getUserLogout: 'auth/getUserLogout',
-		}),
-	},
+    userLogout() {
+      this.getUserLogout().then(() => {
+        this.isLoading = false;
+        localStorage.removeItem("user");
+        this.$router.push({ name: "UserLogin" });
+      });
+    },
+    ...mapActions({
+      getUserLogout: "auth/getUserLogout",
+    }),
+  },
 };
 </script>
 <style scoped>
 .fixed-bar {
-	position: sticky;
-	position: -webkit-sticky; /* for Safari */
-	top: 6em;
-	z-index: 2;
+  position: sticky;
+  position: -webkit-sticky; /* for Safari */
+  top: 6em;
+  z-index: 2;
 }
 </style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,13 +1,15 @@
 <template>
-
   <div>
-    <br>
-    <br>
-    <br>
+    <br />
+    <br />
+    <br />
     <v-card width="400" class="mx-auto mt-7 mb-7">
       <v-card-title style=" margin-bottom:5px;">
-        <span style="color:#ff6d00;margin-left:23%;font-size:xx-large;font-weight:bold
-          ">User Traders</span>
+        <span
+          style="color:#ff6d00;margin-left:23%;font-size:xx-large;font-weight:bold
+          "
+          >User Traders</span
+        >
       </v-card-title>
       <v-card-text>
         <div id="signUpForm">
@@ -15,53 +17,184 @@
             <v-container>
               <v-row>
                 <v-col>
-                  <v-text-field v-model="email" :rules="[rules.required]" label="이메일" hint="ex) jaebin@naver.com" prepend-icon="mdi-account-circle" autocomplete="off" required outlined></v-text-field>
+                  <v-text-field
+                    v-model="email"
+                    :rules="[rules.required]"
+                    label="이메일"
+                    hint="ex) jaebin@naver.com"
+                    prepend-icon="mdi-account-circle"
+                    autocomplete="off"
+                    required
+                    outlined
+                  ></v-text-field>
+
+                  <v-btn
+                    rounded
+                    color="#ff6d00 "
+                    class="ml-9 white--text mr-3"
+                    style="margin-left:70%;"
+                    @click="getcheckedemail"
+                  >
+                    중복검사
+                  </v-btn>
+
+                  <v-icon color="green" v-if="isemailcheked == 1"
+                    >mdi-check-bold</v-icon
+                  >
+                  <v-icon color="red" v-if="isemailcheked == 2"
+                    >mdi-block-helper</v-icon
+                  >
                 </v-col>
               </v-row>
 
               <v-row>
                 <v-col>
-                  <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.minPw]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="비밀번호" hint="적어도 8글자 이상 입력해주세요." prepend-icon="mdi-lock" counter @click:append="show1 = !show1" outlined></v-text-field>
+                  <v-text-field
+                    v-model="password"
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.minPw]"
+                    :type="show1 ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="비밀번호"
+                    hint="적어도 8글자 이상 입력해주세요."
+                    prepend-icon="mdi-lock"
+                    counter
+                    @click:append="show1 = !show1"
+                    outlined
+                  ></v-text-field>
                 </v-col>
               </v-row>
 
               <v-row>
                 <v-col>
-                  <v-text-field v-model="passwordCheck" :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.minPw, rules.pwCheck]" :type="show2 ? 'text' : 'password'" name="input-10-1" label="비밀번호 확인" hint="비밀번호를 한번더 입력해주세요." prepend-icon="mdi-lock" counter @click:append="show2 = !show2" outlined></v-text-field>
+                  <v-text-field
+                    v-model="passwordCheck"
+                    :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.minPw, rules.pwCheck]"
+                    :type="show2 ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="비밀번호 확인"
+                    hint="비밀번호를 한번더 입력해주세요."
+                    prepend-icon="mdi-lock"
+                    counter
+                    @click:append="show2 = !show2"
+                    outlined
+                  ></v-text-field>
                 </v-col>
               </v-row>
 
               <v-row>
                 <v-col>
-                  <v-text-field v-model="name" :rules="[rules.minName]" label="이름" hint="적어도 1글자 이상 입력해주세요." prepend-icon="mdi-account" autocomplete="off" required outlined></v-text-field>
+                  <v-text-field
+                    v-model="name"
+                    :rules="[rules.minName]"
+                    label="이름"
+                    hint="적어도 1글자 이상 입력해주세요."
+                    prepend-icon="mdi-account"
+                    autocomplete="off"
+                    required
+                    outlined
+                  ></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
                 <v-col>
-                  <v-text-field v-model="number" label="휴대폰 번호 -없이 입력" prepend-icon="mdi-cellphone" oninput="javascript: this.value = this.value.replace(/[^0-9]/g, '');" autocomplete="off" outlined></v-text-field>
+                  <v-text-field
+                    v-model="nickname"
+                    :rules="[rules.minNickname]"
+                    label="닉네임"
+                    hint="적어도 1글자 이상 입력해주세요."
+                    prepend-icon="mdi-account-multiple"
+                    autocomplete="off"
+                    required
+                    outlined
+                  ></v-text-field>
+
+                  <v-btn
+                    rounded
+                    color="#ff6d00 "
+                    class="ml-9 white--text"
+                    style="margin-left:70%;"
+                    @click="getcheckednickname"
+                  >
+                    중복검사
+                  </v-btn>
+                  <v-icon color="green" v-if="isnicknamechecked"
+                    >mdi-check-bold</v-icon
+                  >
                 </v-col>
               </v-row>
               <v-row>
                 <v-col>
-                  <v-select v-model="select" :items="items" @click="departmentName" label="학과 선택" prepend-icon="mdi-school" data-vv-name="select" required outlined></v-select>
+                  <v-text-field
+                    v-model="number"
+                    label="휴대폰 번호 -없이 입력"
+                    prepend-icon="mdi-cellphone"
+                    oninput="javascript: this.value = this.value.replace(/[^0-9]/g, '');"
+                    autocomplete="off"
+                    outlined
+                  ></v-text-field>
                 </v-col>
               </v-row>
+              <v-row>
+                <v-col>
+                  <v-select
+                    v-model="select"
+                    :items="items"
+                    @click="departmentId"
+                    label="학과 선택"
+                    prepend-icon="mdi-school"
+                    data-vv-name="select"
+                    required
+                    outlined
+                  ></v-select>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    v-model="studentid"
+                    :rules="[rules.minStudent]"
+                    label="학번"
+                    hint="적어도 1글자 이상 입력해주세요."
+                    prepend-icon="mdi-card-account-details"
+                    autocomplete="off"
+                    required
+                    outlined
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <!-- <h3>성별</h3> -->
+              <v-radio-group
+                v-model="radiogroup"
+                row
+                prepend-icon="mdi-human-male-female"
+              >
+                <v-radio label="남성" value="man" v-model="gender"></v-radio>
+                <v-radio label="여성" value="woman" v-model="gender"></v-radio>
+              </v-radio-group>
             </v-container>
           </v-form>
         </div>
       </v-card-text>
       <v-divider> </v-divider>
       <v-card-actions>
-        <v-btn :disabled="!valid" rounded color="#ff6d00 " class="mr-3 white--text" style="margin-left:70%;" @click="signUpSubmit">
+        <v-btn
+          :disabled="!valid"
+          rounded
+          color="#ff6d00 "
+          class="mr-3 white--text"
+          style="margin-left:70%;"
+          @click="signUpSubmit"
+        >
           회원가입
           <v-icon right>mdi-arrow-right-thick</v-icon>
         </v-btn>
-
       </v-card-actions>
     </v-card>
-    <br>
+    <br />
 
-    <br>
+    <br />
   </div>
 </template>
 <script>
@@ -72,55 +205,153 @@ export default {
       show1: false,
       show2: false,
       email: "",
+      checkEmailString: "",
       password: "",
       passwordCheck: "",
       name: "",
+      nickname: "",
+      studentid: "",
       valid: true,
       items: [],
+      itemsName: [],
       itemsid: [],
       select: "",
       number: "",
+      isnicknamechecked: 0,
+      isemailcheked: false,
+      gender: "",
+      radiogroup: "",
+
       rules: {
-        required: input => !!input || "Required.",
-        minPw: password => password.length >= 8 || "Min 8 characters",
-        minName: Name => Name.length >= 1 || "Min 1 characters",
-        minPhone: number => number.length >= 11 || "Min 11 characters",
-        pwCheck: passwordCheck =>
-          this.password === passwordCheck || "Password mismatch"
-      }
+        required: (input) => !!input || "이메일을 입력해주십시오.",
+        minPw: (password) => password.length >= 8 || "Min 8 characters",
+        minName: (Name) => Name.length >= 1 || "Min 1 characters",
+        minNickname: (nickName) => nickName.length >= 1 || "Min 1 characters",
+        minPhone: (number) => number.length >= 11 || "Min 11 characters",
+        pwCheck: (passwordCheck) =>
+          this.password === passwordCheck || "Password mismatch",
+      },
     };
   },
   watch: {
-    number: function () {
-      return this.number = this.number.replace(/[^0-9]/g, '');
-    }
+    number: function() {
+      return (this.number = this.number.replace(/[^0-9]/g, ""));
+    },
   },
   methods: {
-    departmentName() {
-      return http.process("user", "listdepartment").then((data) => {
-        for (var i = 0; i < data.length; i++) {
-          this.items = this.items.concat(data[i].id + " " + data[i].name)
-        }
-      }).catch(err => {
-        console.log(err)
-      });
+    departmentId() {
+      return http
+        .process("user", "listdepartment")
+        .then((data) => {
+          console.log(data);
+          for (var i = 0; i < data.userDepartmentDtoList.length; i++) {
+            this.items = this.items.concat(
+              data.userDepartmentDtoList[i].id +
+                " " +
+                data.userDepartmentDtoList[i].name
+            );
+          }
+          // for(var i= 0; i < data.userDepartmentDtoList.length; i++){
 
+          // }
+          console.log(this.items);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    getcheckedemail() {
+      let checkEmailString = this.email;
+      let count = 0;
+      for (var i = 0; i < checkEmailString.length; i++) {
+        if (checkEmailString[i] == "@" || checkEmailString[i] == ".") {
+          count++;
+        }
+      }
+      if (!(count >= 2)) {
+        this.isemailcheked = 2;
+        alert("이메일을 형식에 맞게 작성해주십시오.");
+        return;
+      }
+
+      // if (checkEmailString == null || checkEmailString == undefined) {
+
+      //   return;
+      // } else {
+      //   checkEmailString = this.email;
+      // }
+
+      return http
+        .process("user", "emailCheck", {
+          email: checkEmailString,
+        })
+        .then((res) => {
+          console.log(res.check);
+          if (res.check == "중복된 이메일 입니다.") {
+            this.isemailcheked = 2;
+            alert("중복된 이메일 입니다.");
+          } else {
+            this.isemailcheked = 1;
+            // alert("사용가능한 이메일 입니다.");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getcheckednickname() {
+      let checkNicknameString = this.nickname;
+      if (checkNicknameString == null || checkNicknameString == undefined) {
+        return;
+      } else {
+        checkNicknameString = this.nickname;
+      }
+
+      return http
+        .process("user", "nicknameCheck", {
+          nickname: checkNicknameString,
+        })
+        .then((res) => {
+          console.log(res.check);
+          if (res.check == "중복된 닉네임 입니다.") {
+            this.isnicknamechecked = false;
+            alert("중복된 닉네임 입니다.");
+          } else {
+            this.isnicknamechecked = true;
+            // alert("사용가능한 닉네임 입니다.");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     signUpSubmit() {
-      const temp = this.select.split(" ")
-      const department = { id: temp[0] }
-      const userObj = { userid: this.email, password: this.password, tel: this.number, department: department, name: this.name }
-      return http.process("user", "signup", userObj)
-        .then((res) => {
-          console.log(res)
-          this.$router.push({ name: 'UserLogin' })
-        }).catch((err) => { console.log(err) })
-
+      if (this.isemailcheked == true) {
+        const temp = this.select.split(" ");
+        const department = { id: temp[0] };
+        const userObj = {
+          userid: this.email,
+          password: this.password,
+          tel: this.number,
+          department: department,
+          name: this.name,
+          nickname: this.nickname,
+        };
+        return http
+          .process("user", "signup", userObj)
+          .then((res) => {
+            console.log(res);
+            this.$router.push({ name: "UserLogin" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        alert("닉네임이랑 이메일 체크를 꼭하셔야합니다.");
+      }
     },
-
   },
-}
-
+};
 </script>
-<style scoped>
-</style>
+<style scoped></style>
