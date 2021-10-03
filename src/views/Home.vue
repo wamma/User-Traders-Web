39013e668c9e8aba16e49cca02c90ee53f181f72
wamma <pparkheong96@naver.com>
2021-10-03 18:17:28 +0900

    rebase update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,304 +1,304 @@
 <template>
-	<div>
-		<v-container v-if="listData" three-line>
-			<v-row class="mt-16">
-				<v-col
-					v-for="(item, i) in listData"
-					:key="`item-${i}`"
-					cols="12"
-					sm="4"
-				>
-					<div>
-						<v-hover v-slot="{ hover }">
-							<v-card class="ma-2" max-width="344">
-								<div @click="detailPush(item.id)">
-									<div style="text-align : left; color : #fc9942">
-										<v-icon class="pr-2 pl-2" title="카테고리" color="#fc9942">
-											mdi-tag
-										</v-icon>
-										{{ item.categoryId.subCategoryId.name }} >
-										{{ item.categoryId.name }}
-									</div>
-									<v-img
-										v-bind:src="item.thumbnail"
-										:aspect-ratio="11 / 8"
-										height="mx-auto"
-									>
-										<v-expand-transition>
-											<div
-												v-if="hover"
-												class="d-flex transition-fast-in-fast-out orange 
+  <div>
+    <v-container v-if="listData" three-line>
+      <v-row class="mt-16">
+        <v-col
+          v-for="(item, i) in listData"
+          :key="`item-${i}`"
+          cols="12"
+          sm="4"
+        >
+          <div>
+            <v-hover v-slot="{ hover }">
+              <v-card class="ma-2" max-width="344">
+                <div @click="detailPush(item.id)">
+                  <div style="text-align : left; color : #fc9942">
+                    <v-icon class="pr-2 pl-2" title="카테고리" color="#fc9942">
+                      mdi-tag
+                    </v-icon>
+                    {{ item.categoryId.subCategoryId.name }} >
+                    {{ item.categoryId.name }}
+                  </div>
+                  <v-img
+                    v-bind:src="item.thumbnail"
+                    :aspect-ratio="11 / 8"
+                    height="mx-auto"
+                  >
+                    <v-expand-transition>
+                      <div
+                        v-if="hover"
+                        class="d-flex transition-fast-in-fast-out orange 
 												lighten-2 darken-2 v-card--reveal display-3 white--text"
-												style="height: 100%;"
-											></div>
-										</v-expand-transition>
-									</v-img>
-									<div>
-										<v-card-text
-											style="border-bottom:solid; border-width:0.5px; border-color: #cccccc;"
-											><div style="height: 56px; width: 100% ">
-												<div style="width: 50%; float: left; ">
-													<div>
-														<v-list-item-avatar>
-															<v-img v-bind:src="item.userId.imagePath"></v-img>
-														</v-list-item-avatar>
+                        style="height: 100%;"
+                      ></div>
+                    </v-expand-transition>
+                  </v-img>
+                  <div>
+                    <v-card-text
+                      style="border-bottom:solid; border-width:0.5px; border-color: #cccccc;"
+                      ><div style="height: 56px; width: 100% ">
+                        <div style="width: 50%; float: left; ">
+                          <div>
+                            <v-list-item-avatar>
+                              <v-img v-bind:src="item.userId.imagePath"></v-img>
+                            </v-list-item-avatar>
 
-														{{ item.userId.nickName }}
-													</div>
-												</div>
-												<div
-													v-if="item.status == 0"
-													class="light-blue--text"
-													style="margin-left: 50%; text-align: right;"
-													title="판매중"
-												>
-													{{ item.status | tradeStatus }}
-													<v-icon
-														v-if="item.grade == 0"
-														color="#ff0008"
-														title="새상품"
-													>
-														mdi-new-box
-													</v-icon>
-													<v-icon
-														v-if="item.grade == 1"
-														color="#fc0558"
-														title="S급"
-													>
-														mdi-alpha-s-box
-													</v-icon>
-													<v-icon
-														v-else-if="item.grade == 2"
-														color="#e39a07"
-														title="A급"
-													>
-														mdi-alpha-a-box
-													</v-icon>
-													<v-icon
-														v-else-if="item.grade == 3"
-														color="#a6a5a2"
-														title="B급"
-													>
-														mdi-alpha-b-box
-													</v-icon>
-												</div>
-												<div
-													v-else-if="item.status == 1"
-													class="orange--text"
-													style="margin-left: 50%; text-align: right;"
-													title=""
-												>
-													{{ item.status | tradeStatus }}
-													<v-icon
-														v-if="item.grade == 0"
-														color="#ff0008"
-														title="새상품"
-													>
-														mdi-new-box
-													</v-icon>
-													<v-icon
-														v-if="item.grade == 1"
-														color="#fc0558"
-														title="S급"
-													>
-														mdi-alpha-s-box
-													</v-icon>
-													<v-icon
-														v-else-if="item.grade == 2"
-														color="#e39a07"
-														title="A급"
-													>
-														mdi-alpha-a-box
-													</v-icon>
-													<v-icon
-														v-else-if="item.grade == 3"
-														color="#a6a5a2"
-														title="B급"
-													>
-														mdi-alpha-b-box
-													</v-icon>
-												</div>
-												<div
-													v-else-if="item.status == 2"
-													class="gray--text"
-													style="margin-left: 50%; text-align: right;"
-													title=""
-												>
-													{{ item.status | tradeStatus }}
-													<v-icon
-														v-if="item.grade == 0"
-														color="#ff0008"
-														title="새상품"
-													>
-														mdi-new-box
-													</v-icon>
-													<v-icon
-														v-if="item.grade == 1"
-														color="#fc0558"
-														title="S급"
-													>
-														mdi-alpha-s-box
-													</v-icon>
-													<v-icon
-														v-else-if="item.grade == 2"
-														color="#e39a07"
-														title="A급"
-													>
-														mdi-alpha-a-box
-													</v-icon>
-													<v-icon
-														v-else-if="item.grade == 3"
-														color="#a6a5a2"
-														title="B급"
-													>
-														mdi-alpha-b-box
-													</v-icon>
-												</div>
-											</div>
-										</v-card-text>
-									</div>
-									<v-card-text class="pa-1" style="position: relative;">
-										<v-card-text class="mt-2 pa-3">
-											<div
-												class="font-weight-medium"
-												style="font-size:x-large; font-weight:bold"
-											>
-												{{ item.title }}
-											</div>
-										</v-card-text>
-										<v-card-text class="pa-3">
-											<div
-												class="font-weight-medium float-right "
-												style="font-size:large;"
-											>
-												{{ item.price | moneyFilter }} 원
-											</div>
-										</v-card-text>
+                            {{ item.userId.nickName }}
+                          </div>
+                        </div>
+                        <div
+                          v-if="item.status == 0"
+                          class="light-blue--text"
+                          style="margin-left: 50%; text-align: right;"
+                          title="판매중"
+                        >
+                          {{ item.status | tradeStatus }}
+                          <v-icon
+                            v-if="item.grade == 0"
+                            color="#ff0008"
+                            title="새상품"
+                          >
+                            mdi-new-box
+                          </v-icon>
+                          <v-icon
+                            v-if="item.grade == 1"
+                            color="#fc0558"
+                            title="S급"
+                          >
+                            mdi-alpha-s-box
+                          </v-icon>
+                          <v-icon
+                            v-else-if="item.grade == 2"
+                            color="#e39a07"
+                            title="A급"
+                          >
+                            mdi-alpha-a-box
+                          </v-icon>
+                          <v-icon
+                            v-else-if="item.grade == 3"
+                            color="#a6a5a2"
+                            title="B급"
+                          >
+                            mdi-alpha-b-box
+                          </v-icon>
+                        </div>
+                        <div
+                          v-else-if="item.status == 1"
+                          class="orange--text"
+                          style="margin-left: 50%; text-align: right;"
+                          title=""
+                        >
+                          {{ item.status | tradeStatus }}
+                          <v-icon
+                            v-if="item.grade == 0"
+                            color="#ff0008"
+                            title="새상품"
+                          >
+                            mdi-new-box
+                          </v-icon>
+                          <v-icon
+                            v-if="item.grade == 1"
+                            color="#fc0558"
+                            title="S급"
+                          >
+                            mdi-alpha-s-box
+                          </v-icon>
+                          <v-icon
+                            v-else-if="item.grade == 2"
+                            color="#e39a07"
+                            title="A급"
+                          >
+                            mdi-alpha-a-box
+                          </v-icon>
+                          <v-icon
+                            v-else-if="item.grade == 3"
+                            color="#a6a5a2"
+                            title="B급"
+                          >
+                            mdi-alpha-b-box
+                          </v-icon>
+                        </div>
+                        <div
+                          v-else-if="item.status == 2"
+                          class="gray--text"
+                          style="margin-left: 50%; text-align: right;"
+                          title=""
+                        >
+                          {{ item.status | tradeStatus }}
+                          <v-icon
+                            v-if="item.grade == 0"
+                            color="#ff0008"
+                            title="새상품"
+                          >
+                            mdi-new-box
+                          </v-icon>
+                          <v-icon
+                            v-if="item.grade == 1"
+                            color="#fc0558"
+                            title="S급"
+                          >
+                            mdi-alpha-s-box
+                          </v-icon>
+                          <v-icon
+                            v-else-if="item.grade == 2"
+                            color="#e39a07"
+                            title="A급"
+                          >
+                            mdi-alpha-a-box
+                          </v-icon>
+                          <v-icon
+                            v-else-if="item.grade == 3"
+                            color="#a6a5a2"
+                            title="B급"
+                          >
+                            mdi-alpha-b-box
+                          </v-icon>
+                        </div>
+                      </div>
+                    </v-card-text>
+                  </div>
+                  <v-card-text class="pa-1" style="position: relative;">
+                    <v-card-text class="mt-2 pa-3">
+                      <div
+                        class="font-weight-medium"
+                        style="font-size:x-large; font-weight:bold"
+                      >
+                        {{ item.title }}
+                      </div>
+                    </v-card-text>
+                    <v-card-text class="pa-3">
+                      <div
+                        class="font-weight-medium float-right "
+                        style="font-size:large;"
+                      >
+                        {{ item.price | moneyFilter }} 원
+                      </div>
+                    </v-card-text>
 
-										<v-card-text class="mt-2 pa-3">
-											<div style="width: 100%;">
-												<div style="width: 20%; float: left">
-													{{ item.createAt | timeForToday }}
-												</div>
-												<div style="margin-left: 20%; text-align: right">
-													<v-icon color="green">
-														mdi-comment-text-multiple
-													</v-icon>
-													{{ item.commentCount }}
-													<v-icon color="red">
-														mdi-heart
-													</v-icon>
-													{{ item.likeCount }}
-													<v-icon color="blue">
-														mdi-eye-outline
-													</v-icon>
-													{{ item.views }}
-													<v-icon color="yellow">
-														mdi-chat-processing
-													</v-icon>
-													{{ item.chatCount }}
-												</div>
-											</div>
-										</v-card-text>
-									</v-card-text>
-								</div>
-							</v-card>
-						</v-hover>
-					</div>
-				</v-col>
-			</v-row>
-		</v-container>
-		<infinite-loading
-			@infinite="infiniteHandler"
-			spinner="waveDots"
-			forceUseInfiniteWrapper
-			><div slot="no-more" class="ma-10">
-				더이상 게시물이 존재하지 않습니다.
-			</div>
-			<div slot="no-results" class="ma-10">
-				존재 하지 않습니다.
-			</div></infinite-loading
-		>
-		<br />
-		<br />
-		<br />
-	</div>
+                    <v-card-text class="mt-2 pa-3">
+                      <div style="width: 100%;">
+                        <div style="width: 20%; float: left">
+                          {{ item.createAt | timeForToday }}
+                        </div>
+                        <div style="margin-left: 20%; text-align: right">
+                          <v-icon color="green">
+                            mdi-comment-text-multiple
+                          </v-icon>
+                          {{ item.commentCount }}
+                          <v-icon color="red">
+                            mdi-heart
+                          </v-icon>
+                          {{ item.likeCount }}
+                          <v-icon color="blue">
+                            mdi-eye-outline
+                          </v-icon>
+                          {{ item.views }}
+                          <v-icon color="yellow">
+                            mdi-chat-processing
+                          </v-icon>
+                          {{ item.chatCount }}
+                        </div>
+                      </div>
+                    </v-card-text>
+                  </v-card-text>
+                </div>
+              </v-card>
+            </v-hover>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <infinite-loading
+      @infinite="infiniteHandler"
+      spinner="waveDots"
+      forceUseInfiniteWrapper
+      ><div slot="no-more" class="ma-10">
+        더이상 게시물이 존재하지 않습니다.
+      </div>
+      <div slot="no-results" class="ma-10">
+        존재 하지 않습니다.
+      </div></infinite-loading
+    >
+    <br />
+    <br />
+    <br />
+  </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
-import InfiniteLoading from 'vue-infinite-loading';
-import http from '@/utils/http';
-import Formatter from '@/mixin/Formatter';
+import { mapState, mapActions } from "vuex";
+import InfiniteLoading from "vue-infinite-loading";
+import http from "@/utils/http";
+import Formatter from "@/mixin/Formatter";
 
 export default {
-	created() {
-		console.log('게시물 불러오기 시작!');
-	},
-	mixins: [Formatter],
-	data() {
-		return {
-			limit: 1,
-			listData: [],
-		};
-	},
-	components: {
-		InfiniteLoading,
-	},
-	computed: {
-		...mapState({
-			isLogin: (state) => state.auth.isLogin,
-			isLoginError: (state) => state.auth.isLoginError,
-			userInfo: (state) => state.auth.userInfo,
-			profileImg: (state) => state.auth.profileImg,
-			jwt: (state) => state.auth.jwt,
-		}),
-	},
+  created() {
+    console.log("게시물 불러오기 시작!");
+  },
+  mixins: [Formatter],
+  data() {
+    return {
+      limit: 1,
+      listData: [],
+    };
+  },
+  components: {
+    InfiniteLoading,
+  },
+  computed: {
+    ...mapState({
+      isLogin: (state) => state.auth.isLogin,
+      isLoginError: (state) => state.auth.isLoginError,
+      userInfo: (state) => state.auth.userInfo,
+      profileImg: (state) => state.auth.profileImg,
+      jwt: (state) => state.auth.jwt,
+    }),
+  },
 
-	mounted() {
-		this.init();
-	},
-	methods: {
-		infiniteHandler($state) {
-			http
-				.process('boards', 'listPagenation', {
-					limit: this.limit,
-				})
-				.then((res) => {
-					setTimeout(() => {
-						if (res.boardResponseDtoList.length) {
-							this.listData = this.listData.concat(res.boardResponseDtoList);
-							$state.loaded();
-							this.limit += 1;
-						} else {
-							$state.complete();
-							/*alert("더 이상 목록이 존재하지 않습니다.")*/
-						}
-					}, 1000);
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		},
+  mounted() {
+    this.init();
+  },
+  methods: {
+    infiniteHandler($state) {
+      http
+        .process("boards", "listPagenation", {
+          limit: this.limit,
+        })
+        .then((res) => {
+          setTimeout(() => {
+            if (res.boardResponseDtoList.length) {
+              this.listData = this.listData.concat(res.boardResponseDtoList);
+              $state.loaded();
+              this.limit += 1;
+            } else {
+              $state.complete();
+              /*alert("더 이상 목록이 존재하지 않습니다.")*/
+            }
+          }, 1000);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
 
-		init() {
-			console.log('home init...');
-		},
+    init() {
+      console.log("home init...");
+    },
 
-		detailPush(id) {
-			this.$router.push({ name: 'BoardDetail', params: { id: id } });
-		},
-		...mapActions({}),
-	},
+    detailPush(id) {
+      this.$router.push({ name: "BoardDetail", params: { id: id } });
+    },
+    ...mapActions({}),
+  },
 };
 </script>
 <style>
 .v-card--reveal {
-	align-items: center;
-	bottom: 0;
-	justify-content: center;
-	opacity: 0.5;
-	position: absolute;
-	width: 100%;
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.5;
+  position: absolute;
+  width: 100%;
 }
 </style>
--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -1,130 +1,130 @@
 <template>
-	<div>
-		<v-card width="360" class="mx-auto ">
-			<div v-if="!isLoginError">
-				<v-card-title>
-					<h3
-						class="pb-3"
-						style="color:#ff6d00; margin-left:21%;font-size:xx-large;font-weight:bold;
+  <div>
+    <v-card width="360" class="mx-auto ">
+      <div v-if="!isLoginError">
+        <v-card-title>
+          <h3
+            class="pb-3"
+            style="color:#ff6d00; margin-left:21%;font-size:xx-large;font-weight:bold;
             border-bottom:solid; border-width:0.5px; border-color: #cccccc;
           "
-					>
-						User Traders
-					</h3>
-				</v-card-title>
-			</div>
-			<div v-else>
-				<v-card-title style="background-color: #FF3232">
-					<h3 style="color: white">이메일과 비밀번호를 확인해주세요</h3>
-				</v-card-title>
-			</div>
+          >
+            User Traders
+          </h3>
+        </v-card-title>
+      </div>
+      <div v-else>
+        <v-card-title style="background-color: #FF3232">
+          <h3 style="color: white">이메일과 비밀번호를 확인해주세요</h3>
+        </v-card-title>
+      </div>
 
-			<v-card-text>
-				<v-form ref="form" v-model="valid" lazy-validation>
-					<v-text-field
-						autocomplete="off"
-						v-model="email"
-						:rules="emailRules"
-						label="이메일"
-						prepend-icon="mdi-account-circle"
-						required
-					></v-text-field>
-					<v-text-field
-						autocomplete="off"
-						v-model="password"
-						label="비밀번호"
-						:rules="passwordRules"
-						prepend-icon="mdi-lock"
-						type="password"
-					></v-text-field>
-				</v-form>
-			</v-card-text>
-			<v-divider> </v-divider>
-			<v-card-actions>
-				<!-- <v-btn color="grey" class="mr-9 white--text" @click="reset">
+      <v-card-text>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            autocomplete="off"
+            v-model="email"
+            :rules="emailRules"
+            label="이메일"
+            prepend-icon="mdi-account-circle"
+            required
+          ></v-text-field>
+          <v-text-field
+            autocomplete="off"
+            v-model="password"
+            label="비밀번호"
+            :rules="passwordRules"
+            prepend-icon="mdi-lock"
+            type="password"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-divider> </v-divider>
+      <v-card-actions>
+        <!-- <v-btn color="grey" class="mr-9 white--text" @click="reset">
 					Reset
 					<v-icon right>mdi-backup-restore</v-icon>
 				</v-btn> -->
-				<v-btn color="green" class="white--text" @click="reset">
-					비밀번호 찾기
-					<v-icon right>mdi-backup-restore</v-icon>
-				</v-btn>
+        <v-btn color="green" class="white--text" @click="reset">
+          비밀번호 찾기
+          <v-icon right>mdi-backup-restore</v-icon>
+        </v-btn>
 
-				<v-btn
-					:disabled="!valid"
-					color="#ff6d00"
-					class="ml-9 mr-2 white--text"
-					@click="loginSubmit"
-				>
-					로그인
-					<v-icon right>mdi-arrow-right-thick</v-icon>
-				</v-btn>
+        <v-btn
+          :disabled="!valid"
+          color="#ff6d00"
+          class="ml-9 mr-2 white--text"
+          @click="loginSubmit"
+        >
+          로그인
+          <v-icon right>mdi-arrow-right-thick</v-icon>
+        </v-btn>
 
-				<v-btn :to="{ name: 'SignUp' }" color="#ff6d00" class="white--text">
-					회원가입
-					<v-icon right>mdi-account-plus</v-icon>
-				</v-btn>
-			</v-card-actions>
-		</v-card>
-	</div>
+        <v-btn :to="{ name: 'SignUp' }" color="#ff6d00" class="white--text">
+          회원가입
+          <v-icon right>mdi-account-plus</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions } from "vuex";
 export default {
-	//로그인에서 사용해야할 값 초기화(이메일, 비밀번호, 이메일 규칙, 비밀번호 규칙)
-	data: () => ({
-		valid: true,
-		email: '',
-		emailRules: [
-			(v) => !!v || '이메일을 입력해주세요.',
-			(v) => /.+@.+\..+/.test(v) || '이메일 형식에 맞게 입력해주세요.',
-		],
-		password: '',
-		passwordRules: [(v) => !!v || '비밀번호를 입력해주세요.'],
-	}),
-	//state:여러 컴포넌트 간에 공유할 데이터 : 상태
-	//getters ,computed : state 값에 접근하는 속성 미리 연산된 값을 접근하는 속성
-	// mutations : state 값을 변경할 수 있는 유일한 방법이자 메서드, commit으로 동작시킨다.
-	computed: {
-		...mapState({
-			isLogin: (state) => state.users.isLogin,
-			isLoginError: (state) => state.users.isLoginError,
-		}),
-	},
+  //로그인에서 사용해야할 값 초기화(이메일, 비밀번호, 이메일 규칙, 비밀번호 규칙)
+  data: () => ({
+    valid: true,
+    email: "",
+    emailRules: [
+      (v) => !!v || "이메일을 입력해주세요.",
+      (v) => /.+@.+\..+/.test(v) || "이메일 형식에 맞게 입력해주세요.",
+    ],
+    password: "",
+    passwordRules: [(v) => !!v || "비밀번호를 입력해주세요."],
+  }),
+  //state:여러 컴포넌트 간에 공유할 데이터 : 상태
+  //getters ,computed : state 값에 접근하는 속성 미리 연산된 값을 접근하는 속성
+  // mutations : state 값을 변경할 수 있는 유일한 방법이자 메서드, commit으로 동작시킨다.
+  computed: {
+    ...mapState({
+      isLogin: (state) => state.users.isLogin,
+      isLoginError: (state) => state.users.isLoginError,
+    }),
+  },
 
-	methods: {
-		validate() {
-			this.$refs.form.validate();
-		},
-		reset() {
-			this.$refs.form.reset();
-		},
-		loginSubmit() {
-			const loginObj = { email: this.email, password: this.password };
-			if (
-				loginObj.email == '' ||
-				loginObj.password == '' ||
-				loginObj.email == null ||
-				loginObj.password == null ||
-				loginObj.email == undefined ||
-				loginObj.password == undefined
-			) {
-				alert('이메일이나 비밀번호를 입력해주세요.');
-				return;
-			}
-			//로그인 api 호출 시작 -> UserLogin.vue(this.postUserLogin) -> ...mapActions(PostUserLogin) -> auth.js/postUserLogin -> action안의 postUserLogin -> http 함수 호출 -> http 함수는 또 @/util\http/index.js 에 있음 -> http post 메소드를 requestBody와 requestheader 그리고 url과 함께 호출 -> return p 안에 response가 담기고 해당 데이터를 가공 -> 여러 유효성 검사 후 함수 콜백 -> 그리고 지금 .then() -> res를 같이 담아줘서 response값중
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    loginSubmit() {
+      const loginObj = { email: this.email, password: this.password };
+      if (
+        loginObj.email == "" ||
+        loginObj.password == "" ||
+        loginObj.email == null ||
+        loginObj.password == null ||
+        loginObj.email == undefined ||
+        loginObj.password == undefined
+      ) {
+        alert("이메일이나 비밀번호를 입력해주세요.");
+        return;
+      }
+      //로그인 api 호출 시작 -> UserLogin.vue(this.postUserLogin) -> ...mapActions(PostUserLogin) -> auth.js/postUserLogin -> action안의 postUserLogin -> http 함수 호출 -> http 함수는 또 @/util\http/index.js 에 있음 -> http post 메소드를 requestBody와 requestheader 그리고 url과 함께 호출 -> return p 안에 response가 담기고 해당 데이터를 가공 -> 여러 유효성 검사 후 함수 콜백 -> 그리고 지금 .then() -> res를 같이 담아줘서 response값중
 
-			this.postUserLogin(loginObj).then((res) => {
-				console.log(res.token);
-				this.$router.push(this.$route.query.redirect || '/');
-			});
-		},
+      this.postUserLogin(loginObj).then((res) => {
+        console.log(res.token);
+        this.$router.push(this.$route.query.redirect || "/");
+      });
+    },
 
-		...mapActions({
-			getUserLogout: 'auth/getUserLogout',
-			postUserLogin: 'auth/postUserLogin',
-			getUserInfo: 'auth/getUserInfo',
-		}),
-	},
+    ...mapActions({
+      getUserLogout: "auth/getUserLogout",
+      postUserLogin: "auth/postUserLogin",
+      getUserInfo: "auth/getUserInfo",
+    }),
+  },
 };
 </script>
